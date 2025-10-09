02f161dd03cd9287ae01752c666984bcaf18ba84
Your Name <ratoge1816@gmail.com>
2025-10-09 15:13:05 +0800

    change button styling

--- frontend/src/pages/archive.jsx
+++ frontend/src/pages/archive.jsx
@@ -242,7 +242,8 @@ useEffect(() => {
                     user?.role == "head" && (
                       <>
                       <button
-                        className="relative w-[250px] border border-gray-300 rounded-md bg-white text-left px-3 py-2 font-label text-black cursor-pointer appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="relative w-[250px] border border-gray-300 rounded-md bg-white text-left px-3 py-2 font-label text-black cursor-pointer appearance-none
+                                  focus:outline-none active:bg-[#E46455] active:text-white transition-colors duration-150"
                         //onClick=
                       >
                         Select by Time
