24b91550e73b8b3bdcf100e5bd850e37d43c4ebf
vanasher <ratoge1816@gmail.com>
2025-10-10 22:25:11 +0800

    a

--- frontend/src/pages/archive.jsx
+++ frontend/src/pages/archive.jsx
@@ -253,7 +253,7 @@ useEffect(() => {
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
-                          strokeWidth={3}
+                          strokeWidth={4}
                         >
                           <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
                         </svg>
