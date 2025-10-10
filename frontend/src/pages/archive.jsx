e6d03bb612489738cfd877a31f2a5004a6648b5e
vanasher <ratoge1816@gmail.com>
2025-10-10 22:27:59 +0800

    a

--- frontend/src/pages/archive.jsx
+++ frontend/src/pages/archive.jsx
@@ -249,7 +249,7 @@ useEffect(() => {
                         Select by Time
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="absolute right-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-700 pointer-events-none font-bold-label"
+                          className="absolute right-2 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-700 pointer-events-none font-bold-label"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
