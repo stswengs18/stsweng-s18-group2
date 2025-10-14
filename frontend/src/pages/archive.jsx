959dc980006e7106e130610ed8f1524605241ad5
vanasher <ratoge1816@gmail.com>
2025-10-14 08:56:38 +0800

    a

--- frontend/src/pages/archive.jsx
+++ frontend/src/pages/archive.jsx
@@ -437,7 +437,7 @@ useEffect(() => {
                   {/* add options here */}
                 </select>
                 <button
-                  className="text-white text-xl px-5 py-3 rounded-lg hover:bg-[#277195] bg-[#3186B2] transition"
+                  className="text-white text-xl font-semibold px-5 py-3 rounded-lg hover:bg-[#277195] bg-[#3186B2] transition"
                 >
                   Apply
                 </button>
@@ -463,7 +463,7 @@ useEffect(() => {
                     className="border rounded-lg px-7 py-4 w-65 text-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
                   <button
-                    className="text-white text-xl px-5 py-3 rounded-lg hover:bg-[#277195] bg-[#3186B2] transition"
+                    className="text-white text-xl font-semibold px-5 py-3 rounded-lg hover:bg-[#277195] bg-[#3186B2] transition"
                   >
                     Apply
                   </button>
